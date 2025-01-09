fee685237cb3b731c2e6f05c2d1e6e80aa9b62b0
cyyen <36688951+cyyen@users.noreply.github.com>
2025-01-09 16:21:37 +0800

    add home page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ new Vue({
     el: '#app',
     data: {
         // url: 'http://127.0.0.1:8000',
-		url: 'https://mystock-chve.onrender.com',
+		url: 'https://mystock-chve.onrender.com/',
         users: [],
         endDate: '',
         stocks: [],
